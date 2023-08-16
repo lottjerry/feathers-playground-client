cc71c52b3291dbb144f0725db5d7f0da75cf702c
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-08-16 13:11:08 -0500

    Changed socket connection

--- src/fathers-client.js
+++ src/fathers-client.js
@@ -3,7 +3,10 @@ import socketio from '@feathersjs/socketio-client';
 import auth from '@feathersjs/authentication-client';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3030', { transports: ['websocket'] });
+const socket = io(
+	'https://feathers-playground-server-production.up.railway.app/',
+	{ transports: ['websocket'] }
+);
 
 const feathersClient = feathers()
 	.configure(socketio(socket))
